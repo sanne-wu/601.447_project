a7e1914344795a47ce406b706187dcc5d7db7915
silberr <78942141+silberr@users.noreply.github.com>
2024-11-11 20:25:42 -0500

    create matrix

--- minhash.cpp
+++ minhash.cpp
@@ -32,6 +32,7 @@ unordered_set<string> bottom_k_sketch_fromfile(const string& filename,  size_t k
     priority_queue<pair<size_t, string>> pq;
 
     while (genome_input.get(c)) {
+        if (!is_nucleotide(c)) continue;
         if (kmer.length() < kmer_size) {
             kmer.push_back(c);
         } else {
@@ -56,7 +57,7 @@ unordered_set<string> bottom_k_sketch_fromfile(const string& filename,  size_t k
     return sketch;
 }
 
-double estimate_jaccard(const unordered_set<string>& A, const unordered_set<string>& B, hash<string> my_hash, size_t k=0) {
+double estimate_jaccard(const unordered_set<string>& A, const unordered_set<string>& B, hash<string> my_hash, size_t k) {
     if (k == 0) {
         k = max(A.size(), B.size());
     }
@@ -81,4 +82,54 @@ unordered_set<string> make_kmer_set(const string& s, size_t k) {
         kmer_set.insert(s.substr(i, k));
     }
     return kmer_set;
+}
+
+void make_similarity_matrix(const string& filename, size_t kmer_size, hash<string> my_hash, size_t k, ostream& out) {
+    out << "Source: " << filename << endl;
+    ifstream file_list;
+    file_list.open(filename);
+    string file;
+    string name;
+    unordered_map<string, string> name_to_file;
+    vector<string> names;
+    while (file_list >> file >> name) {
+        name_to_file[name] = file;
+        names.push_back(name);
+    }
+    size_t n = names.size();
+    vector<vector<double>> similarity_matrix(n, vector<double>(n, 0));
+    unordered_map<string, unordered_set<string>> sketches;
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j <= i; j++) {
+            if (i == j) {
+                similarity_matrix[i][j] = 1;
+            } else {
+                string A_name = names[i];
+                string B_name = names[j];
+                if (sketches.find(A_name) == sketches.end()) {
+                    sketches[A_name] = bottom_k_sketch_fromfile(name_to_file[A_name], kmer_size, my_hash, k);
+                }
+                if (sketches.find(B_name) == sketches.end()) {
+                    sketches[B_name] = bottom_k_sketch_fromfile(name_to_file[B_name], kmer_size, my_hash, k);
+                }
+                double jaccard_estimate = estimate_jaccard(sketches[A_name], sketches[B_name], my_hash, k);
+                similarity_matrix[i][j] = jaccard_estimate;
+                similarity_matrix[j][i] = jaccard_estimate;
+            }
+        }
+    }
+    for (size_t i = 0; i < n; i++) {
+        out << i << " " << names[i] << endl;
+    }
+
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < n; j++) {
+            out << similarity_matrix[i][j] << "\t";
+        }
+        out << endl;
+    }
+}
+
+bool is_nucleotide(char c) {
+    return (c == 'A' || c == 'T' || c == 'C' || c == 'G');
 }
--- minhash.h
+++ minhash.h
@@ -4,6 +4,9 @@
 #include <utility>
 #include <unordered_set>
 #include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
 
 using std::string;
 using std::hash;
@@ -13,11 +16,21 @@ using std::make_pair;
 using std::unordered_set;
 using std::max;
 using std::ifstream;
+using std::ofstream;
+using std::ostream;
+using std::vector;
+using std::unordered_map;
+using std::cout;
+using std::endl;
 
-unordered_set<string> bottom_k_sketch(const unordered_set<std::string>&, hash<std::string>, size_t);
+unordered_set<string> bottom_k_sketch(const unordered_set<string>& kmers, hash<string> my_hash,  size_t k);
 
-unordered_set<string> bottom_k_sketch_fromfile(const string&, size_t, hash<string>,  size_t);
+unordered_set<string> bottom_k_sketch_fromfile(const string& filename,  size_t kmer_size, hash<string> my_hash,  size_t k);
 
-double estimate_jaccard(const unordered_set<std::string>&, const unordered_set<std::string>&, hash<std::string>, size_t);
+double estimate_jaccard(const unordered_set<string>& A, const unordered_set<string>& B, hash<string> my_hash, size_t k=0);
 
-unordered_set<string> make_kmer_set(const string&, size_t);
+unordered_set<string> make_kmer_set(const string& s, size_t k);
+
+void make_similarity_matrix(const string& filename, size_t kmer_size, hash<string> my_hash, size_t k, ostream& out=cout);
+
+bool is_nucleotide(char c);
