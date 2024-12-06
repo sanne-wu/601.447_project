7afee3c41326e0ba06edd1fccb3bbb25a2897ecc
amanda-x <hxu86@jhu.edu>
2024-12-06 12:35:59 -0500

    changed argument handling for extract_multiple.py

--- extract_multiple.py
+++ extract_multiple.py
@@ -49,13 +49,19 @@ def extract_and_write_features(genome, features, output_file):
 
 def main():
   if len(sys.argv) != 5:
-    print("Usage: python3 extract_multiple.py feature <genome_list.txt> <gtf_list.txt> <output_paths.txt>")
+    print("Usage: python3 extract_line_by_line_with_names.py feature <genome_list.txt> <gtf_list.txt> <output_folder>")
     sys.exit(1)
 
   feature = sys.argv[1]
   genome_list_file = sys.argv[2]
   gtf_list_file = sys.argv[3]
-  output_paths_file = sys.argv[4]
+  output_folder = sys.argv[4]
+
+  if not os.path.exists(output_folder): #check if output folder exists
+    print(f"Output folder '{output_folder}' does not exist. A new folder called '{output_folder}' will be created.")
+    os.makedirs(output_folder)
+
+  output_paths_file = os.path.join(output_folder, f"output_{feature}_paths.txt")
 
   with open(genome_list_file, 'r') as genome_list, open(gtf_list_file, 'r') as gtf_list, open(output_paths_file, 'w') as out_paths:
     for genome_line, gtf_line in zip(genome_list, gtf_list):
@@ -66,7 +72,6 @@ def main():
         print(f"Skipping empty line: genome='{genome_line}', gtf='{gtf_line}'")
         continue
 
-      #parsing input
       genome_name, genome_path = genome_line.split(" ", 1)
       gtf_name, gtf_path = gtf_line.split(" ", 1)
 
@@ -74,9 +79,9 @@ def main():
         print(f"Mismatch in genome and GTF names: {genome_name} vs {gtf_name}")
         continue
 
-      output_file = f"{genome_name}_{feature}_sequences.fna"
+      output_file = os.path.join(output_folder, f"{genome_name}_{feature}_sequences.fna")
 
-      if feature == "genome": #handle genome where extraction is not needed
+      if feature == "genome": #handle genome level where no extraction is needed
         shutil.copy(genome_path, output_file)
       else:
         #process genome and GTF file
@@ -84,7 +89,7 @@ def main():
         genome = load_genome(genome_path, True)
         extract_and_write_features(genome, features, output_file)
 
-      #write output file path with species name and feature to <output_paths.txt>
+      #output path file format: path + species_feature
       out_paths.write(f"{os.path.abspath(output_file)} {genome_name}_{feature}\n")
 
 if __name__ == "__main__":
